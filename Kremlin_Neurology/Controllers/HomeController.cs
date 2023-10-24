61778ecd79f395aaefd04d2798e8c1dc88511fbc
Renzo-Dev <dankoshevoy2001@gmail.com>
2023-10-24 19:51:21 +0300

    update

---
+++ Kremlin_Neurology/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kremlin_Neurology.Controllers;
+
+public class HomeController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
